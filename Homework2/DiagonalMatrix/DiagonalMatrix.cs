9c452eef7817d7d3dfa96e4c0b963f952c4b980f
Dmitrijus Byckovas <boyuda96@gmail.com>
2021-12-16 16:28:46 +0200

    Created class with constructor Testing the code to apply diagonal matrix values to the 2d array

---
+++ Homework2/DiagonalMatrix/DiagonalMatrix.cs
@@ -0,0 +1,47 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace DiagonalMatrix
+{
+    class DiagonalMatrix
+    {
+        public int[] DiagonalNumbers;
+        public int Size { get; private set; }
+        //var newArray = new int[diagonalNumbers.Size, diagonalNumbers.Size];
+
+        public DiagonalMatrix(params int[] diagonalNumbers)
+        {
+            this.DiagonalNumbers = diagonalNumbers;
+
+            //Calculating the size of the Matrix
+            if (diagonalNumbers == null)
+            {
+                Size = 0;
+            }
+            else
+            {
+                foreach (var n in diagonalNumbers)
+                {
+                    Size++;
+                }
+            }
+
+
+        }
+
+        //Calculating the sum of the diagonal numbers
+        public int Track()
+        {
+            var sum = 0;
+            foreach (var number in DiagonalNumbers)
+            {
+                sum += number;
+            }
+
+            return sum;
+        }
+    }
+}
--- Homework2/DiagonalMatrix/Program.cs
+++ Homework2/DiagonalMatrix/Program.cs
@@ -6,7 +6,50 @@ namespace DiagonalMatrix
     {
         static void Main(string[] args)
         {
-            Console.WriteLine("Hello World!");
+            {
+                var diagonalNumbers = new DiagonalMatrix(8, 1, 2, 3, 6);
+                Console.WriteLine(diagonalNumbers.Track());
+
+                Console.WriteLine(diagonalNumbers.Size);
+
+
+                //Testing the code to change Diagonal Values
+
+                var newArray = new int[diagonalNumbers.Size, diagonalNumbers.Size];
+                int[] num = { 8, 1, 2, 3, 6 };
+
+
+                for (int row = 0; row < diagonalNumbers.Size; row++)
+                {
+                    for (int column = 0; column < diagonalNumbers.Size; column++)
+                    {
+                        if (row == column)
+                        {
+                            newArray[row, column] = num[row];
+
+                        }
+                    }
+
+                }
+
+                //Testing function to display Matrix on the console
+
+                static void print(int[,] matrix, int size)
+                {
+                    for (int i = 0; i < size; i++)
+                    {
+                        for (int j = 0; j < size; j++)
+                        {
+                            Console.Write(matrix[i, j] + " ");
+                        }
+
+                        Console.WriteLine();
+                    }
+                }
+
+                print(newArray, diagonalNumbers.Size);
+            }
         }
     }
 }
+
