2ea59abf95cab748ad0f91dcc2d796914d2f1c04
Dmitrijus Byckovas <boyuda96@gmail.com>
2022-01-24 13:20:13 +0200

    added ctor

--- Homework2/TrainingManagementSystem/PracticalLesson.cs
+++ Homework2/TrainingManagementSystem/PracticalLesson.cs
@@ -2,7 +2,7 @@
 
 namespace TrainingManagementSystem
 {
-    class PracticalLesson : LessonBase
+    class PracticalLesson:LessonBase
     {
         public string TaskCondition { get; set; } = "";
         public string TaskSolution { get; set; } = "";
