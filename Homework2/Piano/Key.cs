e8430f75c31f9f959ef462be85bc5c96775f98f5
Dmitrijus Byckovas <boyuda96@gmail.com>
2022-01-25 11:51:00 +0200

    overriding tostring

--- Homework2/Piano/Key.cs
+++ Homework2/Piano/Key.cs
@@ -8,38 +8,43 @@ namespace Piano
 {
     struct Key
     {
-        public Note _Note { get; set; }
-        public Accidental _Accidental { get; set; }
-        public Octave _Octave{ get; set; }
+        public Note note { get; private set; }
+        public Accidental accidental { get; private set; }
+        public Octave octave{ get; private set; }
 
         public Key(Note note, Accidental accidental, Octave octave)
         {
-            _Note = note;
-            _Accidental = accidental;
-            _Octave = octave;
-
+            this.note = note;
+            this.accidental = accidental;
+            this.octave = octave;
         }
 
         public enum Note
         {
-            A,B,C,D,E,F
+            A,B,C,D,E,F,G
         }
 
         public enum Accidental
         {
             Sharp,
-            Flat
+            Flat,
+            NoSign
         }
 
         public enum Octave
         {
-            First,
-            Second,
-            Third,
-            Fourth,
-            Fifth,
-            Sixth,
-            Seventh
+            First = 1,
+            Second = 2,
+            Third = 3,
+            Fourth = 4,
+            Fifth = 5,
+            Sixth = 6,
+            Seventh = 7
+        }
+
+        public override string ToString()
+        {
+            return note + "" + accidental + " " + $"({(int)octave})" ;
         }
     }
 }
