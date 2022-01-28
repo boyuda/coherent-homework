5547388884cfbb532206894217ffd4420e265af4
Dmitrijus Byckovas <boyuda96@gmail.com>
2022-01-28 17:16:18 +0200

    Implemented Dynamic Array Push and IsEmpty methods.

--- Homework2/Stack/Program.cs
+++ Homework2/Stack/Program.cs
@@ -6,6 +6,25 @@ namespace Stack
     {
         static void Main(string[] args)
         {
+            var stackOne = new Stack<int>();
+            stackOne.IsEmpty();
+            var stackTwo = new Stack<string>();
+
+            stackOne.Push(1);
+            stackOne.IsEmpty();
+
+            stackOne.Push(2);
+            stackOne.Push(3);
+            stackOne.Push(4);
+            stackOne.Push(5);
+            stackOne.Push(6);
+            stackOne.Push(7);
+            stackOne.Push(8);
+            stackOne.Push(9);
+            stackOne.Push(10);
+
+
+
         }
     }
 }
---
+++ Homework2/Stack/Stack.cs
@@ -0,0 +1,62 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Stack
+{
+    class Stack<T> : IStack<T>
+    {
+        public int Size { get; set; }
+        private T[] stackArray;
+        public bool isFull { get; set; } = true;
+        public int CurrentPosition { get; set; }
+
+
+        public Stack()
+        {
+            stackArray = new T[Size];
+        }
+
+
+        public bool IsEmpty()
+        {
+            if (Size == 0) 
+                return true;
+            else
+            return false;
+        }
+
+        public void Pop()
+        {
+            throw new NotImplementedException();
+        }
+
+        public void Push(T obj)
+        {
+            if(obj==null)
+            {
+                throw new ArgumentNullException();
+            }
+
+            if(isFull == true)
+            {
+                ArrayGrow();
+            }
+
+            stackArray[CurrentPosition] = obj;
+            CurrentPosition++;
+            isFull = true;
+        }
+
+        public void ArrayGrow()
+        {
+            Size++;
+            T[] newArray = new T[Size];
+            Array.Copy(stackArray, newArray, CurrentPosition);
+            stackArray = newArray; 
+            isFull = false;
+        }
+    }
+}
