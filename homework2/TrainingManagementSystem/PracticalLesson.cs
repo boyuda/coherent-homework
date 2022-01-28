50de2189cef79ebed61432492da271e95fe05e97
Dmitrijus Byckovas <boyuda96@gmail.com>
2022-01-28 14:29:56 +0200

    Clean up

--- homework2/TrainingManagementSystem/PracticalLesson.cs
+++ homework2/TrainingManagementSystem/PracticalLesson.cs
@@ -7,11 +7,17 @@ namespace TrainingManagementSystem
         public string TaskCondition { get; set; } = "";
         public string TaskSolution { get; set; } = "";
 
-        public PracticalLesson(string _taskCondition, string taskSolution, string _description)
+        public PracticalLesson(string _taskCondition, string taskSolution)
         {
             TaskCondition = _taskCondition;
             TaskSolution = taskSolution;
         }
 
+        public override PracticalLesson Clone()
+        {
+            var clonedLecture = new PracticalLesson(this.TaskCondition, this.TaskSolution);
+            clonedLecture.Description = this.Description;
+            return clonedLecture;
+        }
     }
 }
