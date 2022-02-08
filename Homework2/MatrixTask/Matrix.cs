532aaea09fceea1482eb70c4fe0759f9c878f181
Dmitrijus Byckovas <boyuda96@gmail.com>
2022-02-08 13:26:31 +0200

    Adding indexer

---
+++ Homework2/MatrixTask/Matrix.cs
@@ -0,0 +1,68 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace MatrixTask
+{
+    class Matrix<T>
+    {
+        private T[] DiagonalNumbers { get; set; }
+        public int Size { get; private set; }
+
+
+        public Matrix(int size)
+        {
+            if (size < 0)
+            {
+                throw new ArgumentException();
+            }
+            else
+            {
+                Size = size;
+            }
+        }
+
+
+
+        //Adding Indexer
+        public T this[int i, int j]
+        {
+            get
+            {
+                if (i < 0 || j < 0 || i >= Size || j >= Size)
+                {
+                    throw new IndexOutOfRangeException();
+                }
+                else if (i != j)
+                {
+                    return default(T);
+                }
+                else
+                {
+                    return DiagonalNumbers[i];
+                }
+            }
+
+            set
+            {
+                if (i < 0 || j < 0 || i >= Size || j >= Size)
+                {
+                    throw new IndexOutOfRangeException();
+                }
+                else if (i != j)
+                {
+                    // do nothing
+                }
+                else
+                {
+                    DiagonalNumbers[i] = value;
+                }
+            }
+
+
+
+        }
+    }
+}
--- Homework2/MatrixTask/Program.cs
+++ Homework2/MatrixTask/Program.cs
@@ -6,7 +6,16 @@ namespace MatrixTask
     {
         static void Main(string[] args)
         {
-            Console.WriteLine("Hello World!");
+            var matrixOne = new Matrix<int>(5);
+            matrixOne[0, 0] = 5;
+            matrixOne[1, 1] = 5;
+            matrixOne[2, 2] = 5;
+            matrixOne[3, 3] = 5;
+            matrixOne[4, 4] = 5;
+
+
+
+
         }
     }
 }
