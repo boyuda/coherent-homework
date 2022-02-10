b6dfd95c6ef788c0ba8f1e11bd953541e52c90bd
Dmitrijus Byckovas <boyuda96@gmail.com>
2022-02-10 20:10:47 +0200

    Implemented GetNoZeroElements

--- Homework2/SparseMatrix/Program.cs
+++ Homework2/SparseMatrix/Program.cs
@@ -1,12 +1,26 @@
 ﻿using System;
+using System.Collections.Generic;
 
-namespace SparseMatrix
+namespace SparseMatrixTask
 {
     class Program
     {
+
+        //Entry point
         static void Main(string[] args)
         {
-            Console.WriteLine("Hello World!");
+
+ 
+            var matrix = new SparseMatrix(5, 5);
+            matrix[0,0] = 1;
+            matrix[0, 2] = 3;
+            matrix[0, 3] = 4;
+            matrix[0, 4] = 5;
+
+
+            matrix.GetNozeroElements();
+
+
         }
     }
 }
--- Homework2/SparseMatrix/SparseMatrix.cs
+++ Homework2/SparseMatrix/SparseMatrix.cs
@@ -1,4 +1,5 @@
 ﻿using System;
+using System.Collections;
 using System.Collections.Generic;
 using System.Linq;
 using System.Text;
@@ -8,21 +9,85 @@ namespace SparseMatrixTask
 {
     class SparseMatrix
     {
+
+        public int Size { get; set; }
         public int RowSize { get; set; }
         public int ColumnSize { get; set; }
 
-        public int Size { get; set; }
-
         private Dictionary<int, Dictionary<int, int>> Matrix;
+
+
+
 
         public SparseMatrix(int rowSize, int columnSize)
         {
-            if (rowSize > 0 && columnSize > 0)
+            if (rowSize <= 0 && columnSize <= 0)
+            {
+                throw new ArgumentException("Wrong Size");
+            }
+            else
             {
                 RowSize = rowSize;
                 ColumnSize = columnSize;
-                Size = RowSize * ColumnSize;
+                Size = rowSize * columnSize;
                 Matrix = new Dictionary<int, Dictionary<int, int>>(Size);
+            }
+
+        }
+
+
+
+        //Indexer
+        public int this[int i, int j]
+        {
+            get
+            {
+                Dictionary<int, int> cols;
+                if (Matrix.TryGetValue(i, out cols))
+                {
+                    int value = default;
+                    if (cols.TryGetValue(j, out value))
+                        return value;
+                }
+                return default;
+            }
+
+            set
+            {
+                if (i >= RowSize || j >= ColumnSize)
+                {
+                    throw new ArgumentOutOfRangeException();
+                }
+
+
+                Dictionary<int, int> columns;
+
+                if (!Matrix.TryGetValue(i, out columns))
+                {
+                    columns = new Dictionary<int, int>();
+                    Matrix.Add(i, columns);
+                }
+                columns[j] = value;
+
+            }
+        }
+
+        public IEnumerable<int> GetAllElements()
+        {
+            //Not Implemented
+            yield return 0;
+
+        }
+
+
+        public IEnumerable<(int, int, int)> GetNozeroElements()
+        {
+            foreach (var kvp in Matrix)
+            {
+                foreach (var innerKvp in kvp.Value)
+                {
+                    yield return (kvp.Key, innerKvp.Key, innerKvp.Value);
+                }
 
             }
         }
