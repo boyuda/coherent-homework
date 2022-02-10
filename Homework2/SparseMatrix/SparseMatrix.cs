2a7abc1c3b52a1bb76909ab04d9f3d621c318c3f
Dmitrijus Byckovas <boyuda96@gmail.com>
2022-02-10 15:40:13 +0200

    Initial push

---
+++ Homework2/SparseMatrix/SparseMatrix.cs
@@ -0,0 +1,30 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace SparseMatrixTask
+{
+    class SparseMatrix
+    {
+        public int RowSize { get; set; }
+        public int ColumnSize { get; set; }
+
+        public int Size { get; set; }
+
+        private Dictionary<int, Dictionary<int, int>> Matrix;
+
+        public SparseMatrix(int rowSize, int columnSize)
+        {
+            if (rowSize > 0 && columnSize > 0)
+            {
+                RowSize = rowSize;
+                ColumnSize = columnSize;
+                Size = RowSize * ColumnSize;
+                Matrix = new Dictionary<int, Dictionary<int, int>>(Size);
+
+            }
+        }
+    }
+}
