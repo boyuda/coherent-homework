38ab27c31e2af1cda00e7fe1067e42d15ad4aa9e
Dmitrijus Byckovas <boyuda96@gmail.com>
2022-02-19 15:56:34 +0200

    Implemented method to check for vacation date correctness

--- Homework2/VacationTask/Program.cs
+++ Homework2/VacationTask/Program.cs
@@ -12,8 +12,6 @@ namespace VacationTask
             var date = new DateTime(2021, 12, 25);
 
 
-            //var vacation = new Vacation(employeeOne, new DateTime(2021, 01, 15), new DateTime(2021, 01, 16));
-
 
             var vacation = new Vacation();
 
@@ -21,14 +19,15 @@ namespace VacationTask
             vacation.GoOnVacation(employeeOne, new DateTime(2021, 01, 20), new DateTime(2021, 01, 25));
             vacation.GoOnVacation(employeeOne, new DateTime(2021, 01, 15), new DateTime(2021, 01, 16));
             vacation.GoOnVacation(employeeOne, new DateTime(2021, 01, 25), new DateTime(2021, 01, 30));
-            vacation.GoOnVacation(employeeOne, new DateTime(2021, 01, 01), new DateTime(2021, 01, 10));
-            vacation.GoOnVacation(employeeTwo, new DateTime(2021, 02, 15), new DateTime(2021, 02, 18));
-            vacation.GoOnVacation(employeeTwo, new DateTime(2021, 03, 15), new DateTime(2021, 03, 18));
+            vacation.GoOnVacation(employeeOne, new DateTime(2021, 01, 26), new DateTime(2021, 01, 29));
+            vacation.GoOnVacation(employeeOne, new DateTime(2021, 01, 21), new DateTime(2021, 01, 24));
+             vacation.GoOnVacation(employeeTwo, new DateTime(2021, 02, 15), new DateTime(2021, 02, 18));
+             vacation.GoOnVacation(employeeTwo, new DateTime(2021, 03, 15), new DateTime(2021, 03, 18));
             vacation.AverageVacationDays();
             vacation.AverageEmployeeVacationDays();
 
             vacation.EmployeesOnVacationByMonth();
-
+            vacation.MonthsWithNoVacation();
 
 
         }
--- Homework2/VacationTask/Vacation.cs
+++ Homework2/VacationTask/Vacation.cs
@@ -8,16 +8,18 @@ namespace VacationTask
 {
     class Vacation
     {
-        public DateTime FirstDayOfVacation { get; set; }
-        public DateTime LastDayOfVacation { get; set; }
-        public Employee Employee { get; set; }
-        public static List<Vacation> VacationList = new List<Vacation>();
+        public DateTime FirstDayOfVacation { get; private set; }
+        public DateTime LastDayOfVacation { get; private set; }
+        public Employee Employee { get; private set; }
+        private static List<Vacation> VacationList = new List<Vacation>();
 
+        //Empty ctor for initial object creation
         public Vacation()
         {
-            //Used for initial vacation creation
+            //No Implementation
         }
 
+        //Ctor with parameters do add people on vacation to the list
         public Vacation(Employee employee, DateTime firstDay, DateTime lastDay)
         {
             Employee = employee;
@@ -25,8 +27,15 @@ namespace VacationTask
             LastDayOfVacation = lastDay;
         }
 
+        //Method to send person on vacation. adds vacation to the main list and to the personal employee list
         public void GoOnVacation(Employee employee, DateTime firstDay, DateTime lastDay)
         {
+
+            if(TakenVacationCheck(employee,firstDay,lastDay))
+            {
+                throw new ArgumentException("Vacation already taken");
+            }
+
             if(firstDay>lastDay)
             {
                 throw new ArgumentException("First vacation day, cannot be later than the last vacation day");
@@ -41,25 +50,23 @@ namespace VacationTask
             employee.EmployeeVacation.Add(new Vacation(employee, firstDay, lastDay));
         }
 
+        //Method to find the average length of vacation in the organization.
         public double AverageVacationDays()
         {
            double sum = 0;
            double counter = 0;
-           var days = from items in VacationList 
-                      select new
-                       {
-                           days = items.LastDayOfVacation - items.FirstDayOfVacation
-                       };
+
+           var days = VacationList.Select(d => d.LastDayOfVacation - d.FirstDayOfVacation);
 
             foreach (var day in days)
             {
-                sum = sum + day.days.TotalDays;
+                sum += day.Days;
                 counter++;
             }
-
             return sum/counter;
         }
 
+        //Method to find the average length of vacation for each employee
         public void AverageEmployeeVacationDays()
         {
             var AverageEmployeeVacationDays =
@@ -71,7 +78,6 @@ namespace VacationTask
                     AverageDays = NameGroup.Average(x => (x.LastDayOfVacation - x.FirstDayOfVacation).TotalDays)
                 };
 
-
             foreach (var n in AverageEmployeeVacationDays)
             {
                 Console.WriteLine(Tuple.Create(n.Name, n.AverageDays));
@@ -79,10 +85,29 @@ namespace VacationTask
 
         }
 
+        //Method to print a set of tuples "number of the month of the year - the number of employees on vacation
         public void EmployeesOnVacationByMonth()
         {
-            // var results = VacationList.GroupBy(x => x.FirstDayOfVacation.Month).Select(x => new { count = x.Count()}).ToList();
 
+            var vacationsByMonth =
+                from month in Enumerable.Range(1, 12)
+                let key = new { Month = month }
+                join revision in VacationList on key
+                          equals new
+                          {
+                              revision.FirstDayOfVacation.Month
+                          } into g
+                select new { GroupCriteria = key, Count = g.Count() }; 
+
+            foreach (var n in vacationsByMonth)
+            {
+                Console.WriteLine(Tuple.Create(n.GroupCriteria, n.Count));
+            }
+        }
+
+        //Indicate dates in 2021 on which employees did not take vacations
+        public void MonthsWithNoVacation()
+        {
             var vacationsByMonth =
                 from month in Enumerable.Range(1, 12)
                 let key = new { Month = month }
@@ -95,8 +120,23 @@ namespace VacationTask
 
             foreach (var n in vacationsByMonth)
             {
-                Console.WriteLine(Tuple.Create(n.GroupCriteria, n.Count));
+                if(n.Count == 0)
+                {
+                    Console.WriteLine(Tuple.Create(n.GroupCriteria, n.Count));
+                }
             }
         }
+
+        //checking if dates are not already taken by the employee
+        public bool TakenVacationCheck(Employee employee, DateTime firstDay, DateTime lastDay)
+        {
+            var test = VacationList.Where(d => d.Employee == employee && d.FirstDayOfVacation < firstDay && d.LastDayOfVacation > lastDay).Any();
+
+            if (test)
+                return true;
+            return false;
+        }
+
+
     }
 }
