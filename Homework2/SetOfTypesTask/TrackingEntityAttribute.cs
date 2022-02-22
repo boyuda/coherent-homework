9ac40d0ed3899bff2e7dc8eaf3dadc6df0bb5b3f
Dmitrijus Byckovas <boyuda96@gmail.com>
2022-02-22 20:02:45 +0200

    created attributes

---
+++ Homework2/SetOfTypesTask/TrackingEntityAttribute.cs
@@ -0,0 +1,13 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace CustomAttribute
+{
+    [AttributeUsage(AttributeTargets.Class | AttributeTargets.Struct)]
+    class TrackingEntityAttribute:Attribute
+    {
+    }
+}
---
+++ Homework2/SetOfTypesTask/TrackingPropertyAttribute.cs
@@ -0,0 +1,13 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace CustomAttribute
+{   [AttributeUsage(AttributeTargets.Field | AttributeTargets.Property)]
+    class TrackingPropertyAttribute:Attribute
+    {
+        public string PropertyName { get; set; }
+    }
+}
