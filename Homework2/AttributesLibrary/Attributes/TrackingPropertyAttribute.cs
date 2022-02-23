1b008510a0e89a09a69c9cf0adc28c8a00d9f3d9
Dmitrijus Byckovas <boyuda96@gmail.com>
2022-02-23 20:16:08 +0200

    Added class library

--- Homework2/AttributesLibrary/Attributes/TrackingPropertyAttribute.cs
+++ Homework2/AttributesLibrary/Attributes/TrackingPropertyAttribute.cs
@@ -1,12 +1,8 @@
 ﻿using System;
-using System.Collections.Generic;
-using System.Linq;
-using System.Text;
-using System.Threading.Tasks;
 
 namespace CustomAttribute
 {   [AttributeUsage(AttributeTargets.Field | AttributeTargets.Property)]
-    class TrackingPropertyAttribute:Attribute
+    public class TrackingPropertyAttribute:Attribute
     {
         public string PropertyName { get; set; }
     }
--- Homework2/SetOfTypesTask/Program.cs
+++ Homework2/SetOfTypesTask/Program.cs
@@ -4,11 +4,15 @@ namespace CustomAttribute
 {
     class Program
     {
+
+        //Entry point
         static void Main(string[] args)
         {
             var test = new ClassForTesting { FirstName = "Peter", Age = 25, City = "Vilnius" };
-            var log = new Logger("test");
+            var otherTest = new ClassForTesting { FirstName = "Thomas", Age = 30, City = "Kaunas" };
+            var log = new Logger("john");
             log.Track(test);
+            log.Track(otherTest);
         }
     }
 }
